826ad7bb30c1cee6750d1cdbf9b04227f068b89c
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-01 18:51:52 -0400

    Save Data Query Experiment

--- src/cl/dcc/Experiment.java
+++ src/cl/dcc/Experiment.java
@@ -1,9 +1,90 @@
 package cl.dcc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Gonzaloafa on 31-05-2014.
  */
 public class Experiment {
+
+
+    private String  name;
+    private String  typeSequence;
+
+    private List<QueryKDTree> query;
+
+    private int     repetitions;
+    private double  error;
+    private double  average;
+    private double  totalTime;
+
+    public Experiment(String name, String typeSequence) {
+
+        this.name           = name;
+        this.typeSequence   = typeSequence;
+
+        this.repetitions    = 0;
+        this.average        = 0;
+        this.totalTime      = 0;
+
+        this.query          = new ArrayList<QueryKDTree>();
+    }
+
+
+    public void addQuery (QueryKDTree data){
+
+        // Obtenemos valores que ya se han ingresado antes
+        List<Long> listTime = data(data.getSizeArray());
+
+        // Actualizamos valores
+        this.repetitions = data.getRepetitions();
+        this.totalTime=+data.getTime();
+
+        // Calculamos los datos estadisticos
+        this.average = totalTime/repetitions;
+        this.error = standardDeviation(listTime, data.getTime());
+
+        // Agregamos los datos estadisticos al Pojo
+        data.addAverage(this.average);
+        data.addError(this.error);
+
+        this.query.add(data);
+    }
+
+
+    public boolean isLowError(){
+        return  (this.error < 0.05) ? true : false;
+    }
+
+
+
+    private double standardDeviation(List<Long> listTime, long lastTime) {
+
+        double sum = 0;
+
+        for (long time : listTime){
+            sum +=Math.pow(time - average , 2);
+        }
+        sum+= Math.pow(lastTime - average, 2);
+
+        return Math.sqrt(sum / repetitions - 1);
+    }
+
+
+    private List<Long> data(long sizeArray){
+
+        List<Long> listTime = new ArrayList<Long>();
+
+        for (QueryKDTree temp : query) {
+            if (temp.getSizeArray() == sizeArray ){
+                listTime.add(temp.getTime());
+                this.totalTime = temp.getTime();
+            }
+        }
+
+        return listTime;
+    }
 
 
 }
--- src/cl/dcc/QueryKDTree.java
+++ src/cl/dcc/QueryKDTree.java
@@ -10,30 +10,89 @@ public class QueryKDTree {
     private long timeQuery;
     private long accessDisk;
 
+
+    private int     repetitions;
+    private double  error;
+    private double  average;
+
+
     public QueryKDTree(long sizeArray){
-        this.sizeArray = sizeArray;
+
+        this.sizeArray      = sizeArray;
+        this.timeQuery      = 0;
+        this.accessDisk     = 0;
+        this.repetitions    = 0;
+        this.average        = 0;
+        this.error          = 0;
+    }
+
+    public String getHeader(){
+        String data =
+                "Tamano;" +
+                "Tiempo;"+
+                "Accesos;"+
+                "Repeticiones;"+
+                "promedio;"+
+                "Error;";
+        return data;
     }
 
     public String getData(){
         String data =
                 this.sizeArray+";"+
                 this.timeQuery+";"+
-                this.accessDisk;
-
+                this.accessDisk+";"+
+                this.repetitions+";"+
+                this.average+";"+
+                this.error+";";
         return data;
     }
 
 
-    public void setSizeArray(long sizeArray) {
+    public void addSizeArray(long sizeArray) {
         this.sizeArray = sizeArray;
     }
 
-    public void setTimeQuery(long timeQuery) {
+    public void addTimeQuery(long timeQuery) {
         this.timeQuery = timeQuery;
     }
 
-    public void setAccessDisk(long accessDisk) {
+    public void addAccessDisk(long accessDisk) {
         this.accessDisk = accessDisk;
     }
+
+    public void addAverage(double average) {
+        this.average = average;
+    }
+
+    public void addRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void addError(double error) {
+        this.error = error;
+    }
+
+    public long getTime(){
+        return timeQuery;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+
+    public long getSizeArray(){
+        return sizeArray;
+    }
+
 }
 
