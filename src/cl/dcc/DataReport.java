dc9d5064b36321361dda702c49a3d6e2f799fb8a
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-01 19:41:18 -0400

    update DataReport

--- src/cl/dcc/DataReport.java
+++ src/cl/dcc/DataReport.java
@@ -7,49 +7,39 @@ import java.io.*;
     Obtener los datos
 	Generar tablas y todas esas cosas.
 */
-class DataReport {
 
-    String header = "N° repeticiones;Error;Tamano del arreglo;"+
-            "N° comparaciones;Tiempo de ejecucion;"+
-            "Comparaciones promedio;Tiempo promedio";
+public class DataReport {
 
     private PrintWriter writer, summary;
     public boolean console;
 
 
-    public DataReport(Generator.ArrayType arrayType, Algorithms algorithm, boolean console) {
+    public DataReport(Experiment experiment , boolean console) {
 
         // First create the folder
-        File theDir = new File("results");
+        File theDir = new File("Results");
         if (!theDir.exists())
             theDir.mkdir();
 
 
-        String filename = "results/" + algorithm.getName() + "-" + arrayType.type;
+        String filename = "Results/" + experiment.getName() + "-" + experiment.getTypeSequence();
         this.console = console;
 
         try {
-            writer = new PrintWriter(new BufferedWriter(new FileWriter(filename + ".txt", true)));
+            writer  = new PrintWriter(new BufferedWriter(new FileWriter(filename + ".txt", true)));
             summary = new PrintWriter(new BufferedWriter(new FileWriter(filename + "_summary.txt", true)));
 
-            summary.println(header);
-            writer.println(header);
-            if (console)
-                System.out.println("" + header);
         } catch (IOException e) {
             System.out.println("ERROR ABRIENDO ARCHIVO!!!");
         }
     }
 
-    public void makeReport(Statistics statistic) {
-        writer.println("" + statistic);
+    public void makeReport(Experiment experiment) {
+        writer.println("" + experiment.getReport());
         if (console)
-            System.out.println("" + statistic);
+            System.out.println("" + experiment.getReport());
     }
 
-    public void makeSummary(Statistics statistic) {
-        summary.println("" + statistic);
-    }
 
     public void close() {
 
